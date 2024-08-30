24c657425bc3cab225856e3d9444f8b104e1ca28
mincheol07 <150639482+mincheol07@users.noreply.github.com>
2024-08-30 10:48:25 +0900

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from redis import Redis
 from io import BytesIO
 from bson import ObjectId
 
-client = MongoClient('mongodb://admin:VMware1!@172.24.0.61:27017')
+client = MongoClient('mongodb://team3:VMware1!@172.24.0.61:27017')
 db = client['test']  # 사용할 데이터베이스 선택
 users_collection = db['users']  # 사용할 컬렉션 선택
 production = db['production']
